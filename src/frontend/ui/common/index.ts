430b9da9d586025cf79eb566f613e8ef940a316d
EnderDev <kieran@dothq.co>
2021-03-18 19:56:14 +0000

    ✨ Add Switch component

--- src/frontend/ui/common/index.ts
+++ src/frontend/ui/common/index.ts
@@ -1,5 +1,6 @@
 import Button from './button'
 import Checkbox from './checkbox'
 import { Ring } from './ring'
+import { Switch } from './switch'
 
-export { Button, Checkbox, Ring }
+export { Button, Checkbox, Ring, Switch }
